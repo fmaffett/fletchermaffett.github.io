4bc44e71baaa61049c4f5fe0ebf8e923e94788ec
Fletcher Maffett <fletchermaffett@admin-ltm.local>
2016-10-25 18:55:46 -0400

    4th tweak to HW1 wording, ha

--- HW1/99bottles.js
+++ HW1/99bottles.js
@@ -11,7 +11,7 @@
                 "Take one down and pass it around, " + (i-1) + " bottles of beer on the wall.");
         } else if(i == 1) {
             console.log(i + " bottle of beer on the wall, " + i + " bottle of beer. " +
-                "Take one down and pass it around, " + (i-1) + " bottle of beer on the wall.");
+                "Take one down and pass it around, " + (i-1) + " bottles of beer on the wall.");
         } else {
             console.log("No more bottles of beer on the wall, no more bottles of beer. " +
                 "Go to the store and buy some more, " + 99 + " bottles of beer on the wall.");
