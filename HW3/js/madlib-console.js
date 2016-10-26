b6c214237439b5ed515cca101d6cf735ec9c0a11
Fletcher Maffett <fletchermaffett@admin-ltm.local>
2016-10-25 21:04:00 -0400

    HW3 completed; dice program added as well

--- HW3/js/madlib-console.js
+++ HW3/js/madlib-console.js
@@ -1,17 +1,15 @@
-var startupX = ['Uber', 'Google', 'Amazon', 'Apple', 'Facebook', 'Twitter', 'Inform', 'Microsoft'];
-var startupY = ['Slack', 'Trello', 'Tesla', 'Hyperloop', 'Harvest'];
-
-var random1 = Math.floor((Math.random() * startupX.length));
-var random2 = Math.floor((Math.random() * startupY.length));
+var startupX = ['Uber', 'Augmedix', 'WeGo', 'Lyft', 'Zappos', 'CraigsList', 'Google', 'Amazon', 'Apple', 'Facebook', 'Twitter', 'Inform', 'Microsoft'];
+var startupY = ['Slack', 'Trello', 'Tesla', 'Hyperloop', 'Harvest', 'HipChat', 'GitHub', 'NetBeans', 'Maps', 'Yelp', 'Epicurious'];
 
 document.getElementById("create").onclick = function() {myFunction()};
 
 function myFunction() {
-    document.getDocumentById("xForY").innerHTML = 'A startup that is ' + startupX[random1] + ', but for ' + startupY[random2];
-}
 
-/*
-function myFunction2() {
-    document.getDocumentById("favorites").innerHTML = 'A startup that is ' + startupX[random1] + ', but for ' + startupY[random2];
+    var random1 = Math.floor((Math.random() * startupX.length));
+    var random2 = Math.floor((Math.random() * startupY.length));
+
+    console.log(startupX);
+    console.log(startupY);
+
+    document.getElementById('xForY').innerHTML = 'A startup that is ' + startupX[random1] + ', but for ' + startupY[random2] + '.';
 }
-*/
